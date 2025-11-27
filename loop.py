3f4f612bc7c8a4b81a823b40710298b1b858856c
Vimalraj <vimalraj.murugesan@gmail.com>
2025-11-27 16:48:44 +0530

    for loop max number from a list

--- loop.py
+++ loop.py
@@ -1,1 +1,10 @@
-asdfasdf
+import random
+
+dummy_data = [138, 15, 159, 124, 15, 19, 195, 68, 8, 175, 126, 180, 95, 2, 68, 125, 31, 6, 29,205,112, 152]
+
+max = 0
+for data in dummy_data:
+    if data > max:
+        max = data
+    
+print(max)
