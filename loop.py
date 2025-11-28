a6c5f413666a5515c4ad18e12f3880748680549a
Vimalraj <vimalraj.murugesan@gmail.com>
2025-11-28 15:19:02 +0530

    for loop complete

--- loop.py
+++ loop.py
@@ -1,7 +1,7 @@
 import random
 
 dummy_data = [138, 15, 159, 124, 15, 19, 195, 68, 8, 175, 126, 180, 95, 2, 68, 125, 31, 6, 29,205,112, 152]
-
+# for loop for a list
 max = 0
 for data in dummy_data:
     if data > max:
@@ -10,8 +10,8 @@ for data in dummy_data:
 print(max)
 
 
+# for loop with range function
 sum = 0
-
 for i in range(1,101):
     sum += i
 
